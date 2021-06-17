4705ce44a57692ecf5229591087f0f08fda65218
dikiamust <agungmustaqimdiki@gmail.com>
2021-06-18 06:49:48 +0700

    express & server.js added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home/home', pathMatch: 'full' },
+  {
+    path: 'home',
+    loadChildren: () =>
+      import('./home/home.module').then((route) => route.HomeModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/home/home.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './home/home.component';
+import { AngularComponent } from './angular/angular.component';
+import { NavigationComponent } from '../shared/navigation/navigation.component';
+import { BannerComponent } from './banner/banner.component';
+import { FooterComponent } from '../shared/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    AngularComponent,
+    NavigationComponent,
+    BannerComponent,
+    FooterComponent,
+  ],
+  imports: [CommonModule, HomeRoutingModule],
+})
+export class HomeModule {}
